785c6a29871cd79fd37b61754099de6903e7ee74
Andrii Vdovychenko <fisher377164@gmail.com>
2023-08-12 02:42:09 +0300

    Add some projects content.

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,31 @@
 import { Col } from 'react-bootstrap'
 import PropTypes from 'prop-types'
+import './ProjectCard.css'
 
-const ProjectCard = ({ title, description, imgUrl }) => {
+const ProjectCard = ({ title, role, description,technologies, url, imgUrl }) => {
     return (
         <Col size={12} sm={6} md={4}>
-            <div className="proj-imgbx">
-                <img src={imgUrl} alt="Simple text" />
-                <div className="proj-txtx">
-                    <h4>{title}</h4>
-                    <span>{description}</span>
+            <a href={url} target="_blank" rel="noreferrer">
+                <div className="project-card">
+                    <img src={imgUrl} alt="Simple text" />
+                    <div className="proj-txtx">
+                        <h4>{title}</h4>
+                        <span className="role">{role}</span><br/>
+                        <span className="description">{description}</span><br/>
+                        <span className="technologies">Tools & technologies: {technologies}</span>
+                    </div>
                 </div>
-            </div>
+            </a>
         </Col>
     )
 }
 
 ProjectCard.propTypes = {
     title: PropTypes.string,
+    role: PropTypes.string,
     description: PropTypes.string,
+    technologies: PropTypes.string,
+    url: PropTypes.string,
     imgUrl: PropTypes.string,
 }
 
